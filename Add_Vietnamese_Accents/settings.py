fcbb63e97eda30ee75ba94c2f3a69cdf1e6fb8ac
vanhau201 <conghau12a5@gmail.com>
2022-03-10 18:34:22 +0700

    update

--- Add_Vietnamese_Accents/settings.py
+++ Add_Vietnamese_Accents/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'Add_Vietnamese_Accents.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
